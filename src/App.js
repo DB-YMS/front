a1f845e5148c9a45798d9168ab556398cc5f3f9e
kkeunii <greenlee55@naver.com>
2024-12-01 21:03:26 +0900

    feat: 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "../src/components/login";
 import SignupPage from "../src/components/signup"; // 경로 수정: "..." 대신 "../src" 사용
 import Commute from '../src/components/commute';
 import EquipmentCheck from "../src/components/EquimentCheck";
 import MissionPage from "../src/components/MissionPage";
+import Yard from "../src/components/Yard"
+import Mobile from "../src/components/mobile"
 
 function App() {
   return (
     <div>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignupPage />}></Route>
+          <Route path="/mobile" element={<Mobile />}></Route>
+          <Route path="/mobile/mission" element={<MissionPage />}></Route>
+          <Route path="/mobile/equipmentcheck" element={<EquipmentCheck />}></Route>
+          <Route path="/mobile/commute" element={<Commute />}></Route>
+          <Route path="/yard" element={<Yard />}></Route>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
   
--- src/components/ButtonGrid.js
+++ src/components/ButtonGrid.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import CustomButton from "./Button";
+import CustomButton from "./button";
 
 const ButtonGrid = () => {
   const [selectedButton, setSelectedButton] = useState(null);
